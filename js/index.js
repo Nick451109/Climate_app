24dc68c433f3102c5618080d6aa46dffeabb932b
Nick451109 <narevalo@espol.edu.ec>
2023-06-19 14:46:49 -0500

    added proxies to inocar fecth url for CORS policy

--- js/index.js
+++ js/index.js
@@ -1,43 +1,47 @@
 let plot = (data) => {
-  const ctx = document.getElementById('myChart');
+  const ctx = document.getElementById("myChart");
 
   const dataset = {
-    labels: data.hourly.time, /* ETIQUETA DE DATOS */
-    datasets: [{
-        label: 'Temperatura semanal', /* ETIQUETA DEL GRÁFICO */
-        data: data.hourly.temperature_2m, /* ARREGLO DE DATOS */
+    labels: data.hourly.time /* ETIQUETA DE DATOS */,
+    datasets: [
+      {
+        label: "Temperatura semanal" /* ETIQUETA DEL GRÁFICO */,
+        data: data.hourly.temperature_2m /* ARREGLO DE DATOS */,
         fill: false,
-        borderColor: 'rgb(75, 192, 192)',
-        tension: 0.1
-    }]
+        borderColor: "rgb(75, 192, 192)",
+        tension: 0.1,
+      },
+    ],
   };
   const config = {
-    type: 'line',
+    type: "line",
     data: dataset,
   };
-  const chart = new Chart(ctx, config)
-}
+  const chart = new Chart(ctx, config);
+};
 
 let bar = (data) => {
-  const ctx = document.getElementById('myChart2');
+  const ctx = document.getElementById("myChart2");
 
   const dataset = {
-    labels: data.daily.time, /* ETIQUETA DE DATOS */
-    datasets: [{
-        label: 'Temperatura semanal', /* ETIQUETA DEL GRÁFICO */
-        data: data.daily.uv_index_max, /* ARREGLO DE DATOS */
+    labels: data.daily.time /* ETIQUETA DE DATOS */,
+    datasets: [
+      {
+        label: "Temperatura semanal" /* ETIQUETA DEL GRÁFICO */,
+        data: data.daily.uv_index_max /* ARREGLO DE DATOS */,
         fill: false,
-        borderColor: 'rgb(20, 20, 20)',
-        tension: 0.1
-    }]
+        borderColor: "rgb(20, 20, 20)",
+        tension: 0.1,
+      },
+    ],
   };
   const config = {
-    type: 'bar',
+    type: "bar",
     data: dataset,
   };
-  const chart = new Chart(ctx, config)
-}
-//cargar localmente 
+  const chart = new Chart(ctx, config);
+};
+//cargar localmente
 let load = (data) => {
   console.log(data);
   let latitud = data["latitude"]; //52.52
@@ -50,33 +54,46 @@ let load = (data) => {
   document.getElementById("timezone").innerText = timezone;
   document.getElementById("elevation").innerText = elevation;
   plot(data); //llamada funcion plot
-  bar(data)
- }
+  bar(data);
+};
 
+//load inocar
+let loadInocar = () => {
+  let URL_proxy = "http://localhost:8080/"; // Coloque el URL de acuerdo con la opción de proxy
+  let URL = URL_proxy + "https://www.inocar.mil.ec/mareas/consultan.php";
 
-(function(){
-  let meteo = localStorage.getItem('meteo');
-  if(meteo == null) {
-    let URL = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&daily=uv_index_max&timezone=America%2FLos_Angeles';
-        
-    fetch(URL)
-    .then(response => response.json())
-    .then(data => {
-      
-        load(data)
-        //load()
-        /* GUARDAR DATA EN LA MEMORIA */
-        localStorage.setItem("meteo", JSON.stringify(data))
-
+  fetch(URL)
+    .then((response) => response.text())
+    .then((data) => {
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(data, "text/html");
+      console.log(xml);
+      let contenedorMareas = xml.getElementsByTagName("div")[0];
+      let contenedorHTML = document.getElementById("table-container");
+      contenedorHTML.innerHTML = contenedorMareas.innerHTML;
     })
     .catch(console.error);
+};
 
+(function () {
+  let meteo = localStorage.getItem("meteo");
+  if (meteo == null) {
+    let URL =
+      "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&daily=uv_index_max&timezone=America%2FLos_Angeles";
+
+    fetch(URL)
+      .then((response) => response.json())
+      .then((data) => {
+        load(data);
+
+        //load()
+        /* GUARDAR DATA EN LA MEMORIA */
+        localStorage.setItem("meteo", JSON.stringify(data));
+      })
+      .catch(console.error);
   } else {
-
-      /* CARGAR DATA DESDE LA MEMORIA */
-      load(JSON.parse(meteo))
-
+    /* CARGAR DATA DESDE LA MEMORIA */
+    load(JSON.parse(meteo));
+    loadInocar();
   }
 })();
-
-
