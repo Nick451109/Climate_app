4a293dc285a70b261b544611b5cb6e03dbb0975b
Nick451109 <narevalo@espol.edu.ec>
2023-07-14 21:38:30 -0500

    fixes

---
+++ js/buttons.js
@@ -0,0 +1,9 @@
+let elementos = document.getElementsByClassName("botoncito");
+
+for (let i = 0; i < elementos.length; i++) {
+    const element = elementos[i];
+    console.log("hola");
+
+    // Realiza acciones con cada elemento aquí
+    console.log(element);
+  }
